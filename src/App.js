8a5791b39447acbd84f24edb81d4f1d2782737bb
Irene Guijarro <irene@devaway.es>
2021-06-02 16:17:27 +0200

    fix CI build errors

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import "./App.css";
 const App = () => {
   const [genres, setGenres] = useState(null);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   const fetchData = async () => {
     const response = await fetch("/.netlify/functions/getGenres");
     const responseBody = await response.json();
     setGenres(responseBody.data.reference_list.values);
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>{genres && genres.map((genre) => <Section genre={genre.value} />)}</>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Card from "./Card";
 
 const Section = ({ genre }) => {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const response = await fetch("/.netlify/functions/getMovies", {
       method: "POST",
       body: JSON.stringify({ genre: genre }),
     });
     const responseBody = await response.json();
     setMovies(responseBody.data.movies_by_genre.values);
-  };
+  }, [genre]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <>
